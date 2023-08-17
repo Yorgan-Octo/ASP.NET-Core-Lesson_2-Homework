94ba6075ffb0048605f20d26a0cfed1ab99211e8
Artem Priadchenko <yorgan.info@gmail.com>
2023-08-18 01:32:24 +0300

    Добавьте файлы проекта.

---
+++ ASP.NET-Core-Lesson_2-Homework/Controllers/CalkController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Lesson_2_Homework.Controllers
+{
+    public class CalkController : Controller
+    {
+        public IActionResult Add(int x, int y)
+        {
+            int res = x + y;
+            string textRes = $"Результата {x} + {y} будет {res}";
+
+            return View("Resalt", textRes as object);
+        }
+
+        public IActionResult Mul(int x, int y)
+        {
+            int res = x * y;
+            string textRes = $"Результата {x} * {y} будет {res}";
+
+            return View("Resalt", textRes as object);
+        }
+
+
+        public IActionResult Div(int x, int y)
+        {
+            int res = x / y;
+            string textRes = $"Результата {x} / {y} будет {res}";
+
+            return View("Resalt", textRes as object);
+        }
+
+
+        public IActionResult Sub(int x, int y)
+        {
+            int res = x - y;
+            string textRes = $"Результата {x} - {y} будет {res}";
+
+            return View("Resalt", textRes as object);
+        }
+    }
+}
---
+++ ASP.NET-Core-Lesson_2-Homework/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.Security.Policy;
+
+namespace ASP.NET_Core_Lesson_2_Homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+    }
+}
